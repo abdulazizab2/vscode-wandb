c4900ead290adc8ce09a99a352731ba8bf5831ed
abdulaziz <albuainaina8@gmail.com>
2023-07-03 05:56:06 +0300

    setup template and TODOS for basic functionality of the extension

--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,18 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import * as wandb from '@wandb/sdk';
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "vscode-wandb" is now active!');
+   let disposable = vscode.commands.registerCommand('extension.showExperiments', async () => {
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('vscode-wandb.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from vscode-wandb!');
-	});
+      // Set up the WandB API
+    //   const api = new wandb.Api();  #  TODO: get user API by prompt if required
 
-	context.subscriptions.push(disposable);
+      // Fetch experiments
+    //   const runs = await api.runs('user/project');  #  TODO: fetch any experiment/run from WandB by http requests or sdk if available
+
+    //   showExperimentTable(runs);  #  TODO:  make the implementation
+   });
+
+   context.subscriptions.push(disposable);
 }
-
-// This method is called when your extension is deactivated
-export function deactivate() {}
---
+++ src/table.ts
@@ -0,0 +1,24 @@
+import * as vscode from 'vscode';
+
+// #  TODO: better explicit typing
+export function showExperimentTable(runs: any) {
+    const panel = vscode.window.createWebviewPanel(
+        'experimentTable',
+        'Experiment Table',
+        vscode.ViewColumn.One,
+    );
+
+    let html = '<table>';
+
+    // Add table headers
+    html += '<tr><th>Experiment Name</th><th>Metrics</th></tr>';
+
+    // Add a row for each experiment
+    for (const run of runs) {
+        html += `<tr><td>${run.name}</td><td>${JSON.stringify(run.config.metrics)}</td></tr>`;
+    }
+
+    html += '</table>';
+
+    panel.webview.html = html;
+}
