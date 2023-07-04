e3168680d84d6640d885c0a4a209084debb0ccf6
abdulaziz <albuainaina8@gmail.com>
2023-07-04 06:45:47 +0300

    add hello world example: register wandb api key

--- src/extension.ts
+++ src/extension.ts
@@ -2,17 +2,27 @@ import * as vscode from 'vscode';
 import * as wandb from '@wandb/sdk';
 
 export function activate(context: vscode.ExtensionContext) {
+  let disposable = vscode.commands.registerCommand(
+    'vscode-wandb.registerAPIKey',
+    async () => {
+      const apiKey = await vscode.window.showInputBox({
+        placeHolder: 'Please enter your WandB API key',
+        ignoreFocusOut: true, // Keeps the input box open when losing focus
+      });
 
-   let disposable = vscode.commands.registerCommand('extension.showExperiments', async () => {
+      if (apiKey) {
+      } else {
+        vscode.window.showErrorMessage('You need to provide an API key!');
+      }
 
-      // Set up the WandB API
-    //   const api = new wandb.Api();  #  TODO: get user API by prompt if required
+      // TODO: use apiKey to authenticate with WandB API and verify valid connection
 
       // Fetch experiments
-    //   const runs = await api.runs('user/project');  #  TODO: fetch any experiment/run from WandB by http requests or sdk if available
+      //   const runs = await api.runs('user/project');  #  TODO: fetch any experiment/run from WandB by http requests or sdk if available
 
-    //   showExperimentTable(runs);  #  TODO:  make the implementation
-   });
+      //   showExperimentTable(runs);  #  TODO:  make the implementation
+    }
+  );
 
-   context.subscriptions.push(disposable);
+  context.subscriptions.push(disposable);
 }
--- src/table.ts
+++ src/table.ts
@@ -2,23 +2,25 @@ import * as vscode from 'vscode';
 
 // #  TODO: better explicit typing
 export function showExperimentTable(runs: any) {
-    const panel = vscode.window.createWebviewPanel(
-        'experimentTable',
-        'Experiment Table',
-        vscode.ViewColumn.One,
-    );
+  const panel = vscode.window.createWebviewPanel(
+    'experimentTable',
+    'Experiment Table',
+    vscode.ViewColumn.One
+  );
 
-    let html = '<table>';
+  let html = '<table>';
 
-    // Add table headers
-    html += '<tr><th>Experiment Name</th><th>Metrics</th></tr>';
+  // Add table headers
+  html += '<tr><th>Experiment Name</th><th>Metrics</th></tr>';
 
-    // Add a row for each experiment
-    for (const run of runs) {
-        html += `<tr><td>${run.name}</td><td>${JSON.stringify(run.config.metrics)}</td></tr>`;
-    }
+  // Add a row for each experiment
+  for (const run of runs) {
+    html += `<tr><td>${run.name}</td><td>${JSON.stringify(
+      run.config.metrics
+    )}</td></tr>`;
+  }
 
-    html += '</table>';
+  html += '</table>';
 
-    panel.webview.html = html;
+  panel.webview.html = html;
 }
