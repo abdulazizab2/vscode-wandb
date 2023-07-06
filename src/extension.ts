7384d48d9762d1956718e3cd13f1576707490ab4
abdulaziz <albuainaina8@gmail.com>
2023-07-06 21:35:03 +0300

    prompt user for api key and placeholder for api validation

--- src/extension.ts
+++ src/extension.ts
@@ -1,28 +1,39 @@
 import * as vscode from 'vscode';
 import * as wandb from '@wandb/sdk';
 
-export function activate(context: vscode.ExtensionContext) {
-  let disposable = vscode.commands.registerCommand(
-    'vscode-wandb.registerAPIKey',
-    async () => {
-      const apiKey = await vscode.window.showInputBox({
-        placeHolder: 'Please enter your WandB API key',
-        ignoreFocusOut: true, // Keeps the input box open when losing focus
-      });
+export async function activate(context: vscode.ExtensionContext) {
+  await context.globalState.update('apiKey', undefined); // workaround for resetting API key value during extension activation
+  // Check if API key is in global state
+  let apiKey = context.globalState.get<string>('apiKey');
 
-      if (apiKey) {
-      } else {
-        vscode.window.showErrorMessage('You need to provide an API key!');
+  // If API key doesn't exist or is invalid, prompt the user for it
+  while (!apiKey || !(await isValidApiKey(apiKey))) {
+    apiKey = await vscode.window.showInputBox({
+      placeHolder: 'Please enter your WandB API key',
+      ignoreFocusOut: true, // Keeps the input box open when losing focus
+    });
+
+    if (apiKey && (await isValidApiKey(apiKey))) {
+      // Store API key in global state
+      await context.globalState.update('apiKey', apiKey);
+      const fetchingMessage = vscode.window.setStatusBarMessage(
+        'Fetching projects...'
+      );
+
+      try {
+        // get all user projects
+      } finally {
+        fetchingMessage.dispose(); // Hide the status bar message
       }
-
-      // TODO: use apiKey to authenticate with WandB API and verify valid connection
-
-      // Fetch experiments
-      //   const runs = await api.runs('user/project');  #  TODO: fetch any experiment/run from WandB by http requests or sdk if available
-
-      //   showExperimentTable(runs);  #  TODO:  make the implementation
+    } else {
+      vscode.window.showErrorMessage('You need to provide a valid API key!');
     }
-  );
+  }
+}
 
-  context.subscriptions.push(disposable);
+async function isValidApiKey(apiKey: string): Promise<boolean> {
+  // TODO: replace with your actual validation logic
+  // This could involve making a request to the API and checking if it succeeds, or calling a method from the SDK
+
+  return true;
 }
