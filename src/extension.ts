9507266980317ebaec5336f331a159538bddb6e0
abdulaziz <albuainaina8@gmail.com>
2023-07-07 06:43:24 +0300

    add pseudo-populated tree view that opens a tab with pseudo-results on click

--- src/extension.ts
+++ src/extension.ts
@@ -23,11 +23,33 @@ export async function activate(context: vscode.ExtensionContext) {
       );
 
       try {
-        vscode.window.showInformationMessage('API Key Validated'); //  TODO: remove after getting projects
         //
         // TODO: get all user projects. This is not straightforward as wandb-js doesnt support this natively as of date
         // either use python API with call via HTTP requests (FastAPI as a wrapper)
         // or scrape wandb after successful API authentifcation
+        // or using bare HTTP requests supported by wandb
+        // then add to tree
+        vscode.window.showInformationMessage('API Key Validated'); //  TODO: remove after getting projects
+        const wandbTreeProvider = new WandbTreeProvider();
+        context.subscriptions.push(
+          vscode.window.registerTreeDataProvider(
+            'projectsView',
+            wandbTreeProvider
+          )
+        );
+
+        context.subscriptions.push(
+          vscode.commands.registerCommand(
+            'extension.openProject',
+            async (projectName: string) => {
+              const doc = await vscode.workspace.openTextDocument({
+                language: 'markdown',
+                content: `# Project: ${projectName}\n\nDetails of the project...`,
+              });
+              vscode.window.showTextDocument(doc, { preview: false }); // TODO:  show some results, images or anything for proof of concept
+            }
+          )
+        );
       } finally {
         fetchingMessage.dispose(); // Hide the status bar message
       }
@@ -46,5 +68,55 @@ async function isValidApiKey(apiKey: string): Promise<boolean> {
     return true;
   } catch {
     return false;
+  }
+}
+
+class WandbTreeItem extends vscode.TreeItem {
+  constructor(
+    public readonly label: string,
+    public readonly collapsibleState: vscode.TreeItemCollapsibleState,
+    public readonly command?: vscode.Command
+  ) {
+    super(label, collapsibleState);
+    this.command = {
+      command: 'extension.openProject',
+      title: 'Open Project',
+      arguments: [this.label],
+    };
+  }
+
+  contextValue = 'wandbTreeItem';
+}
+
+class WandbTreeProvider implements vscode.TreeDataProvider<WandbTreeItem> {
+  private _onDidChangeTreeData: vscode.EventEmitter<
+    WandbTreeItem | undefined | null | void
+  > = new vscode.EventEmitter<WandbTreeItem | undefined | null | void>();
+  readonly onDidChangeTreeData: vscode.Event<
+    WandbTreeItem | undefined | null | void
+  > = this._onDidChangeTreeData.event;
+
+  refresh(): void {
+    this._onDidChangeTreeData.fire();
+  }
+
+  getTreeItem(element: WandbTreeItem): vscode.TreeItem {
+    return element;
+  }
+
+  getChildren(element?: WandbTreeItem): Thenable<WandbTreeItem[]> {
+    // Create an empty array to store our project items
+    let projects: WandbTreeItem[] = [];
+
+    // Loop to create 5 project items
+    for (let i = 1; i <= 2; i++) {
+      let project = new WandbTreeItem(
+        `Project ${i}`,
+        vscode.TreeItemCollapsibleState.None
+      );
+      projects.push(project);
+    }
+
+    return Promise.resolve(projects);
   }
 }
